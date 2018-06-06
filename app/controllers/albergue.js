7221108b6922e0ffcc3a4574f6e2ebe3c2c36dc6
Hector Hurtarte <hectorh30@gmail.com>
2018-06-06 17:26:42 -0600

    wip

---
+++ app/controllers/albergue.js
@@ -0,0 +1,8 @@
+import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+
+export default Controller.extend({
+  query: null,
+
+  searchProperties: ['nombre', 'recursosPrioritarios']
+});
